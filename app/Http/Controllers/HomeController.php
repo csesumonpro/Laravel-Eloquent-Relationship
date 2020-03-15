40a9cba36bd6a306e226e22c3242bbb8b9b3e000
csesumonpro <csesumonpro@gmail.com>
2020-03-15 12:36:05 +0600

    One to one added

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Nid;
+use App\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class HomeController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Contracts\Support\Renderable
+     */
+    public function index()
+    {
+        return view('home');
+    }
+    public function userInfo()
+    {
+        $nid = Nid::where('user_id',Auth::user()->id)->first();
+        return view('welcome',compact('nid'));
+    }
+}
